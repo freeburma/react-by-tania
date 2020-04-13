51a507f809fcf38712391c8b498076913addc22e
freeburma <kophyoblue@gmail.com>
2020-04-13 14:58:45 +1200

    Updates

---
+++ precache-manifest.45748275f7be7f3e5bde532fb7394c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3e66dcdc885f1ef1ec49784f756765",
+    "url": "/react-by-tania/index.html"
+  },
+  {
+    "revision": "b674570585ad102eb3f2",
+    "url": "/react-by-tania/static/css/main.5f99f8b7.chunk.css"
+  },
+  {
+    "revision": "0b93b44e3f0d7e3c5d22",
+    "url": "/react-by-tania/static/js/2.eb3e6766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-by-tania/static/js/2.eb3e6766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b674570585ad102eb3f2",
+    "url": "/react-by-tania/static/js/main.8ba47ff4.chunk.js"
+  },
+  {
+    "revision": "aea7048dced4819fc351",
+    "url": "/react-by-tania/static/js/runtime-main.8e0a6d50.js"
+  }
+]);
